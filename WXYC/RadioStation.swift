7c800d90a1a6b358764ac70e4e151a0b02e61e27
Jake Bromberg <jake.bromberg@gmail.com>
2017-11-03 17:38:19 -0700

    refactoring the mega view controller (MVC)

--- WXYC/RadioStation.swift
+++ WXYC/RadioStation.swift
@@ -1,30 +1,7 @@
-import UIKit
-
-//*****************************************************************
-// Radio Station
-//*****************************************************************
-
-// Class inherits from NSObject so that you may easily add features
-// i.e. Saving favorite stations to CoreData, etc
-
-class RadioStation: NSObject {
-    
-    var stationName     : String
-    var stationStreamURL: String
-    var stationImageURL : String
-    var stationDesc     : String
-    var stationLongDesc : String
-    
-    init(name: String, streamURL: String, imageURL: String, desc: String, longDesc: String) {
-        self.stationName      = name
-        self.stationStreamURL = streamURL
-        self.stationImageURL  = imageURL
-        self.stationDesc      = desc
-        self.stationLongDesc  = longDesc
-    }
-    
-    // Convenience init without longDesc
-    convenience init(name: String, streamURL: String, imageURL: String, desc: String) {
-        self.init(name: name, streamURL: streamURL, imageURL: imageURL, desc: desc, longDesc: "")
-    }
+struct RadioStation {
+    let name     : String
+    let streamURL: String
+    let imageURL : String
+    let desc     : String
+    let longDesc : String
 }
---
+++ WXYC/WXYC.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+extension URL {
+    static var WXYCPlaylist: URL {
+        return URL(string: "http://wxyc.info/playlists/recentEntries?v=2&n=15")!
+    }
+    
+    static var WXYCStream: URL {
+        return URL(string: "http://audio-mp3.ibiblio.org:8000/wxyc.mp3")!
+    }
+}
+    
+protocol PlaylistItem: Comparable {
+    var id: Int { get }
+    var hour: Int { get }
+    var chronOrderID: Int { get }
+}
+
+extension PlaylistItem {
+    static func ==(lhs: Self, rhs: Self) -> Bool {
+        return lhs.chronOrderID == rhs.chronOrderID
+    }
+    
+    static func <(lhs: Self, rhs: Self) -> Bool {
+        return lhs.chronOrderID < rhs.chronOrderID
+    }
+}
+
+struct Playcut: PlaylistItem, Codable {
+    let id: Int
+    let hour: Int
+    let chronOrderID: Int
+    let request: Bool = false
+    let songTitle: String
+    let labelName: String
+    let artistName: String
+    let releaseTitle: String
+}
+
+struct Talkset: PlaylistItem, Codable {
+    let id: Int
+    let hour: Int
+    let chronOrderID: Int
+}
+
+struct Breakpoint: PlaylistItem, Codable {
+    let id: Int
+    let hour: Int
+    let chronOrderID: Int
+}
+
+struct Playlist: Codable {
+    let playcuts: [Playcut]
+    let talksets: [Talkset]
+    let breakpoints: [Breakpoint]
+}
