e0a1fb13913d8c7336587387f63b50d90d8b3cfa
Jake Bromberg <jake.bromberg@gmail.com>
2017-11-13 14:21:39 -0800

    polishing InfoDetailViewController

--- WXYC/InfoDetailViewController.swift
+++ WXYC/InfoDetailViewController.swift
@@ -1,50 +1,52 @@
-import UIKit
 import MessageUI
 
-class InfoDetailViewController: UIViewController, MFMailComposeViewControllerDelegate {
-    
-    @IBOutlet weak var stationImageView: UIImageView!
-    @IBOutlet weak var stationNameLabel: UILabel!
-    @IBOutlet weak var stationDescLabel: UILabel!
+class InfoDetailViewController: UIViewController {
     @IBOutlet weak var stationLongDescTextView: UITextView!
-    @IBOutlet weak var okayButton: UIButton!
     @IBOutlet weak var feedbackButton: UIButton!
     
     var currentStation: RadioStation!
 
-    //*****************************************************************
-    // MARK: - ViewDidLoad
-    //*****************************************************************
-    
+    // MARK: Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         stationLongDescTextView.text = currentStation.longDesc
     }
     
-    func configureMailController() -> MFMailComposeViewController {
+    // MARK: IBActions
+
+    @IBAction func feedbackButtonPressed(_ sender: UIButton) {
+        if MFMailComposeViewController.canSendMail() {
+            let mailComposeViewController = stationFeedbackMailController()
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Can't send mail", message: "Looks like you need to add an email address. Go to Settings.", preferredStyle: .alert)
+
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+
+            alert.addAction(UIAlertAction(title: "Settings", style: .default, handler: { _ in
+                let settingsURL = URL(string: UIApplicationOpenSettingsURLString)!
+                UIApplication.shared.open(settingsURL)
+            }))
+
+            present(alert, animated: true, completion: nil)
+        }
+    }
+}
+
+extension InfoDetailViewController: MFMailComposeViewControllerDelegate {
+    private func stationFeedbackMailController() -> MFMailComposeViewController {
         let mailComposerVC = MFMailComposeViewController()
         mailComposerVC.mailComposeDelegate = self
         mailComposerVC.setToRecipients(["dvd@wxyc.org"])
         mailComposerVC.setSubject("Feedback on the WXYC app")
         return mailComposerVC
-        }
+    }
+
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true, completion: nil)
-        }
-    
-    //*****************************************************************
-    // MARK: - IBActions
-    //*****************************************************************
-    
-    @IBAction func okayButtonPressed(_ sender: UIButton) {
-        _ = navigationController?.popViewController(animated: true)
-    }
-    @IBAction func feedbackButtonPressed(_ sender: UIButton) {
-        let mailComposeViewController = configureMailController()
-        if MFMailComposeViewController.canSendMail() {
-            self.present(mailComposeViewController, animated: true, completion: nil)
-            } else {
-            }
     }
 }
