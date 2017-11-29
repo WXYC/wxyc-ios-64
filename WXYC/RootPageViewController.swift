474866a709455d3d1ed117996c22ec8805889638
Jake Bromberg <jake.bromberg@gmail.com>
2017-11-28 20:37:29 -0800

    now showing page dots

--- WXYC/RootPageViewController.swift
+++ WXYC/RootPageViewController.swift
@@ -1,50 +1,69 @@
 import UIKit
 
 class RootPageViewController: UIPageViewController {
-    let nowPlayingViewController: NowPlayingViewController
-    let infoDetailViewController: InfoDetailViewController
+    let nowPlayingViewController = NowPlayingViewController.loadFromNib()
+    let infoDetailViewController = InfoDetailViewController.loadFromNib()
     
     let nowPlayingService: NowPlayingService
-    let lockscreenInfoService: LockscreenInfoService
+    let lockscreenInfoService = LockscreenInfoService()
     
-    let webservice: Webservice
+    var pages: [UIViewController] {
+        return [
+            nowPlayingViewController,
+            infoDetailViewController
+        ]
+    }
+    
+    let webservice = Webservice()
+    
+    // MARK: Life cycle
 
     required init?(coder: NSCoder) {
-        nowPlayingViewController = NowPlayingViewController.loadFromNib()
-        infoDetailViewController = InfoDetailViewController.loadFromNib()
-        
         nowPlayingService = NowPlayingService(delegate: nowPlayingViewController)
-        lockscreenInfoService = LockscreenInfoService()
         
-        webservice = Webservice()
-
         super.init(coder: coder)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.setUpBackground()
+        self.setUpPages()
+        self.setUpPlaylistPolling()
+    }
+    
+    private func setUpBackground() {
         let backgroundImageView = UIImageView(image: #imageLiteral(resourceName: "background"))
         view.insertSubview(backgroundImageView, at: 0)
         backgroundImageView.frame = view.bounds
-
-        self.setViewControllers([nowPlayingViewController],
-                                direction: .forward,
-                                animated: false,
-                                completion: nil)
-
+    }
+    
+    private func setUpPages() {
         self.dataSource = self
         
+        self.setViewControllers(
+            [nowPlayingViewController],
+            direction: .forward,
+            animated: false,
+            completion: nil
+        )
+    }
+    
+    private func setUpPlaylistPolling() {
         _ = Timer.scheduledTimer(timeInterval: 30, target: self, selector: #selector(self.checkPlaylist), userInfo: nil, repeats: true)
         
         self.checkPlaylist()
     }
+    
+    // MARK - Customization
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
-    @objc func checkPlaylist() {
+    // MARK: Playlist stuff
+    
+    @objc private func checkPlaylist() {
         let playcutRequest = webservice.getCurrentPlaycut()
         playcutRequest.observe(with: self.updateWith(playcutResult:))
         
@@ -52,14 +71,14 @@ class RootPageViewController: UIPageViewController {
         imageRequest.observe(with: self.update(artworkResult:))
     }
     
-    func updateWith(playcutResult result: Result<Playcut>) {
+    private func updateWith(playcutResult result: Result<Playcut>) {
         DispatchQueue.main.async {
             self.nowPlayingService.updateWith(playcutResult: result)
             self.lockscreenInfoService.updateWith(playcutResult: result)
         }
     }
     
-    func update(artworkResult: Result<UIImage>) {
+    private func update(artworkResult: Result<UIImage>) {
         DispatchQueue.main.async {
             self.nowPlayingService.update(artworkResult: artworkResult)
             self.lockscreenInfoService.update(artworkResult: artworkResult)
@@ -84,6 +103,14 @@ extension RootPageViewController: UIPageViewControllerDataSource {
         default:
             return nil
         }
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return self.pages.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
     }
 }
 
