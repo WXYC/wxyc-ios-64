dc217b1d9dba001107caf022f5fea7d9d52b7ec8
Jake Bromberg <jake.bromberg@gmail.com>
2017-12-08 21:10:21 -0800

    apple-watch: turns out the track field on iTunes search results is optional. Also, conditionally using the track field if the album title isn't present

--- WXYC/iTunes.swift
+++ WXYC/iTunes.swift
@@ -8,7 +8,7 @@ struct iTunes {
         
         struct Item: Codable {
             let artistName: String
-            let trackName: String
+            let trackName: String?
             let artworkUrl100: URL
         }
     }
@@ -16,10 +16,18 @@ struct iTunes {
     static func searchURL(`for` playcut: Playcut) -> URL {
         var components = URLComponents(string: "https://itunes.apple.com")!
         components.path = "/search"
-        components.queryItems = [
-            URLQueryItem(name: "term", value: playcut.artistName + " " + playcut.songTitle),
-            URLQueryItem(name: "entity", value: "song")
-        ]
+        
+        if let album = playcut.releaseTitle {
+            components.queryItems = [
+                URLQueryItem(name: "term", value: "\(playcut.artistName) \(album)"),
+                URLQueryItem(name: "entity", value: "album")
+            ]
+        } else {
+            components.queryItems = [
+                URLQueryItem(name: "term", value: "\(playcut.artistName) \(playcut.songTitle)"),
+                URLQueryItem(name: "entity", value: "song")
+            ]
+        }
         
         return components.url!
     }
