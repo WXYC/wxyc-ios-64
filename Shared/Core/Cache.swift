64edf1b0e54c955511373e5247c6e98175b3c787
Jake Bromberg <jake.bromberg@gmail.com>
2018-02-18 20:22:50 -0800

    Fixing compiler bug for Codable types with default values

--- Shared/Core/Cache.swift
+++ Shared/Core/Cache.swift
@@ -3,10 +3,17 @@ import Foundation
 let defaultLifespan: TimeInterval = 30
 
 struct CachedRecord<Value: Codable>: Codable {
-    
     let value: Value
-    let timestamp: TimeInterval = Date.timeIntervalSinceReferenceDate
+    let timestamp: TimeInterval
     let lifespan: TimeInterval
+    
+    // Before you're tempted to say, "Swift autosynthesizes initializers on structs, let's blow this away," let me tell you
+    // this: I put this here as a fix to a compiler bug that overrode the timestamp on serialized records. Beware.
+    init(value: Value, timestamp: TimeInterval = Date.timeIntervalSinceReferenceDate, lifespan: TimeInterval) {
+        self.value = value
+        self.timestamp = timestamp
+        self.lifespan = lifespan
+    }
     
     var isExpired: Bool {
         return Date.timeIntervalSinceReferenceDate - self.timestamp > self.lifespan
