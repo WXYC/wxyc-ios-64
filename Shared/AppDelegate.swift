bdf4abf701a89dce0e2352edbc8c03bdfed3c757
Jake Bromberg <jakebromberg@google.com>
2022-01-18 11:52:18 -0800

    master: WIP

--- Shared/AppDelegate.swift
+++ Shared/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow()
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = RootPageViewController()
+        
         try! AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
         
         #if os(iOS)
@@ -28,18 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.nowPlayingObservation = NowPlayingService.shared.observe { nowPlayingItem in
             MPNowPlayingInfoCenter.default().update(nowPlayingItem: nowPlayingItem)
             WidgetCenter.shared.reloadAllTimelines()
-        }
-        
-        WidgetCenter.shared.getCurrentConfigurations { result in
-            guard case let .success(configurations) = result else {
-                return
-            }
-            
-            for configuration in configurations {
-                print(configuration)
-            }
-            
-            WidgetCenter.shared.reloadAllTimelines()
+            self.donate(nowPlayingItem: nowPlayingItem)
         }
         
         return true
@@ -86,6 +79,26 @@ extension AppDelegate {
             await self.cacheCoordinator.set(value: true, for: UserSettingsKeys.intentDonated, lifespan: .distantFuture)
         }
     }
+    
+    func donate(nowPlayingItem: NowPlayingItem) {
+        Task {
+            let mediaItem = INMediaItem(nowPlayingItem)
+            let intent = INSearchForMediaIntent(mediaItems: [mediaItem], mediaSearch: nil)
+            intent.suggestedInvocationPhrase = "What's on WXYC?"
+            
+            let activity = NSUserActivity(nowPlayingItem)
+            activity.isEligibleForSearch = true
+            activity.isEligibleForPrediction = true
+            
+            let response = INSearchForMediaIntentResponse(code: .success, userActivity: activity)
+            let interaction = INInteraction(intent: intent, response: response)
+            do {
+                try await interaction.donate()
+            } catch {
+                print(error)
+            }
+        }
+    }
 
     func shouldDonateSiriIntent() async -> Bool {
         do {
@@ -96,10 +109,87 @@ extension AppDelegate {
     }
 
     func application(_ application: UIApplication, handle intent: INIntent, completionHandler: @escaping (INIntentResponse) -> Void) {
-        RadioPlayerController.shared.play()
+        switch intent.identifier {
+        case IntentIdentifiers.PlayWXYC:
+            RadioPlayerController.shared.play()
+            Task {
+                let nowPlayingItem = await NowPlayingService.shared.fetch()
+                let response = INPlayMediaIntentResponse(code: .success, userActivity: NSUserActivity(nowPlayingItem))
+                response.nowPlayingInfo = [
+                    MPMediaItemPropertyArtist : nowPlayingItem?.playcut.artistName as Any,
+                    MPMediaItemPropertyTitle: nowPlayingItem?.playcut.songTitle as Any,
+                    MPMediaItemPropertyAlbumTitle: nowPlayingItem?.playcut.releaseTitle as Any,
+                ]
+                completionHandler(response)
+            }
+        default:
+            return
+        }
+    }
+}
+
+extension NSUserActivity {
+    convenience init(_ nowPlayingItem: NowPlayingItem?) {
+        switch nowPlayingItem {
+        case .some(let nowPlayingItem):
+            self.init(activityType: NSUserActivityTypeBrowsingWeb)
+            let url: String! = "https://www.google.com/search?q=\(nowPlayingItem.playcut.artistName)+\(nowPlayingItem.playcut.songTitle)"
+                .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+            self.webpageURL = URL(string: url)
+        case .none:
+            self.init(activityType: NSUserActivityTypeBrowsingWeb)
+            self.webpageURL = URL(string: "https://wxyc.org")!
+        }
+    }
+}
+
+enum IntentIdentifiers {
+    static let PlayWXYC = "com.wxyc.ios.intent.play"
+    static let WhatsPlayingOnWXYC = "com.wxyc.ios.intent.whatsPlayingOnWXYC"
+}
+
+extension INInteraction {
+    
+    static var playWXYC: Self {
+        let mediaItem = INMediaItem(
+            identifier: IntentIdentifiers.PlayWXYC,
+            title: "Play",
+            type: .musicStation,
+            artwork: nil
+        )
         
-        let response = INPlayMediaIntentResponse(code: .success, userActivity: nil)
-        completionHandler(response)
+        let intent = INPlayMediaIntent(
+            mediaItems: [mediaItem],
+            mediaContainer: nil,
+            playShuffled: nil,
+            playbackRepeatMode: .none,
+            resumePlayback: false
+        )
+        
+        intent.suggestedInvocationPhrase = "Play WXYC"
+        return Self(intent: intent, response: nil)
+    }
+}
+
+extension INMediaItem {
+    convenience init(_ nowPlayingItem: NowPlayingItem) {
+        self.init(
+            identifier: IntentIdentifiers.WhatsPlayingOnWXYC,
+            title: nowPlayingItem.playcut.songTitle,
+            type: .song,
+            artwork: INImage(nowPlayingItem.artwork),
+            artist: nowPlayingItem.playcut.artistName
+        )
+    }
+}
+
+extension INImage {
+    convenience init?(_ image: UIImage?) {
+        if let artworkData = image?.pngData() {
+            self.init(imageData: artworkData)
+        } else {
+            return nil
+        }
     }
 }
 
--- iOS/InfoDetailViewController.swift
+++ iOS/InfoDetailViewController.swift
@@ -2,24 +2,61 @@ import MessageUI
 import Core
 
 class InfoDetailViewController: UIViewController {
-    @IBOutlet weak var stationDescriptionTextView: UITextView!
+    // MARK: Overrides
     
-    // MARK: Lifecycle
+    override func loadView() {
+        super.loadView()
+        self.view.backgroundColor = .blue
+//        self.view.translatesAutoresizingMaskIntoConstraints = false
+        
+
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        let stationDescriptionTextView = UITextView()
+        stationDescriptionTextView.translatesAutoresizingMaskIntoConstraints = false
         stationDescriptionTextView.text = RadioStation.WXYC.description
+//        stationDescriptionTextView.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+
+        self.view.addSubview(stationDescriptionTextView)
+        NSLayoutConstraint.activate([
+            stationDescriptionTextView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            stationDescriptionTextView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            stationDescriptionTextView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            stationDescriptionTextView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+
+//        let feedbackButton = UIButton(configuration: .plain(), primaryAction: UIAction(handler: self.feedbackButtonPressed))
+//        feedbackButton.setTitle("Send us feedback on the app", for: .normal)
+//        feedbackButton.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.5)
+//
+//        self.view.addSubview(feedbackButton)
+//        self.view.addConstraints([
+//            feedbackButton.heightAnchor.constraint(equalToConstant: 50),
+//            self.view.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: feedbackButton.leadingAnchor),
+//            self.view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: feedbackButton.trailingAnchor),
+//            stationDescriptionTextView.bottomAnchor.constraint(equalTo: feedbackButton.topAnchor, constant: 16),
+//        ])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
     
     // MARK: IBActions
 
-    @IBAction func feedbackButtonPressed(_ sender: UIButton) {
+    func feedbackButtonPressed(_ sender: UIAction) {
         if MFMailComposeViewController.canSendMail() {
             let mailComposeViewController = stationFeedbackMailController()
             self.present(mailComposeViewController, animated: true, completion: nil)
         } else {
-            let alert = UIAlertController(title: "Can't send mail", message: "Looks like you need to add an email address. Go to Settings.", preferredStyle: .alert)
+            let alert = UIAlertController(
+                title: "Can't send mail",
+                message: "Looks like you need to add an email address. Go to Settings.",
+                preferredStyle: .alert
+            )
 
             alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in
                 alert.dismiss(animated: true, completion: nil)
@@ -34,7 +71,7 @@ class InfoDetailViewController: UIViewController {
         }
     }
     
-    @IBAction func popBack(_ sender: UIBarButtonItem) {
+    func popBack(_ sender: UIBarButtonItem) {
         self.navigationController?.popViewController(animated: true)
     }
 }
