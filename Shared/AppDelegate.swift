b64868a2a4fe9ac394da952240047c09ba012ab0
Jake Bromberg <jake.bromberg@gmail.com>
2023-01-07 14:35:00 -0800

    Adding CarPlay stuff

--- Shared/AppDelegate.swift
+++ Shared/AppDelegate.swift
@@ -104,3 +104,10 @@ extension AppDelegate {
 }
 
 #endif
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+    }
+}
