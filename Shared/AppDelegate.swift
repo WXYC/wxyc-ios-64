a24291bba0101024624bf9675b7c311486b3af8f
Jake Bromberg <jake.bromberg@gmail.com>
2023-01-12 14:58:02 -0800

    We need to delete the old intents from SiriKit in order for Siri to respond to the new App Intents intents.

--- Shared/AppDelegate.swift
+++ Shared/AppDelegate.swift
@@ -1,92 +1,97 @@
 import UIKit
 import Combine
 import Core
-import Intents
 import UI
 import MediaPlayer
 import WidgetKit
-import CarPlay
-import SwiftUI
 
 
 @UIApplicationMain
-@MainActor
 class AppDelegate: UIResponder, UIApplicationDelegate {
     let cacheCoordinator = CacheCoordinator.WXYCPlaylist
+  
     var nowPlayingObservation: Any?
-    
+    var shouldDonateSiriIntentObservation: Cancellable?
+
     // MARK: UIApplicationDelegate
     
     var window: UIWindow?
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let nowPlayingInfoCenter = MPNowPlayingInfoCenter.default()
-        
-        if nowPlayingInfoCenter.nowPlayingInfo == nil {
-            nowPlayingInfoCenter.nowPlayingInfo = [:]
-        }
-        
-        nowPlayingInfoCenter.nowPlayingInfo?[MPNowPlayingInfoPropertyIsLiveStream] = true
-        
         try! AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
         
-#if os(iOS)
-        // Make status bar white
-        UINavigationBar.appearance().barStyle = .black
-        Task { await SiriService.shared.donateSiriIntentIfNeeded() }
-#endif
+        #if os(iOS)
+            // Make status bar white
+            UINavigationBar.appearance().barStyle = .black
+            // Siri intents are deprecated in favor of the App Intents framework. See Intents.swift.
+            self.removeDonatedSiriIntentIfNeeded()
+        #endif
         
         self.nowPlayingObservation = NowPlayingService.shared.observe { nowPlayingItem in
-            nowPlayingInfoCenter.update(nowPlayingItem: nowPlayingItem)
+            MPNowPlayingInfoCenter.default().update(nowPlayingItem: nowPlayingItem)
             WidgetCenter.shared.reloadAllTimelines()
-            Task { await SiriService.shared.donate(nowPlayingItem: nowPlayingItem)}
-            CPNowPlayingTemplate.shared.userInfo = nowPlayingItem.toUserInfo()
+        }
+        
+        WidgetCenter.shared.getCurrentConfigurations { result in
+            guard case let .success(configurations) = result else {
+                return
+            }
+            
+            for configuration in configurations {
+                print(configuration)
+            }
+            
+            WidgetCenter.shared.reloadAllTimelines()
         }
         
         return true
     }
-    
+
     func applicationWillTerminate(_ application: UIApplication) {
         UIApplication.shared.endReceivingRemoteControlEvents()
     }
-    
-#if os(iOS)
-    func application(_ application: UIApplication, handle intent: INIntent) async -> INIntentResponse {
-        return await SiriService.shared.handle(intent: intent)
-    }
-#endif
+}
 
-extension AppDelegate: CPTemplateApplicationSceneDelegate, CPNowPlayingTemplateObserver {
-    static var interfaceController: CPInterfaceController?
+#if os(iOS)
+import Intents
+
+extension AppDelegate {
+    enum UserSettingsKeys: String {
+        case intentDonated
+    }
     
-    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
-                                  didConnect interfaceController: CPInterfaceController) {
-        Self.interfaceController = interfaceController
-        
-        Self.interfaceController?.setRootTemplate(CPNowPlayingTemplate.shared, animated: true) { success, error in
-            print("success: \(success), error: \(error)")
+    func removeDonatedSiriIntentIfNeeded() {
+        Task {
+            guard try await self.shouldRemoveSiriIntent() else {
+                return
+            }
+            
+            try await INInteraction.deleteAll()
+            await self.cacheCoordinator.set(
+                value: nil as Bool?,
+                for: UserSettingsKeys.intentDonated,
+                lifespan: .distantFuture
+            )
         }
     }
+    
+    func shouldRemoveSiriIntent() async throws -> Bool {
+        try await !self.cacheCoordinator.value(for: UserSettingsKeys.intentDonated)
+    }
 
-    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
-                                  didDisconnectInterfaceController interfaceController: CPInterfaceController) {
-        Self.interfaceController = nil
+    func application(_ application: UIApplication, handle intent: INIntent, completionHandler: @escaping (INIntentResponse) -> Void) {
+        RadioPlayerController.shared.play()
+        
+        let response = INPlayMediaIntentResponse(code: .success, userActivity: nil)
+        completionHandler(response)
     }
 }
 
-class LoggerWindowSceneDelegate: NSObject, UIWindowSceneDelegate {
-    
-    internal var window: UIWindow?
-    
-    // MARK: UISceneDelegate
-    
+#endif
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene, session.configuration.name == "LoggerSceneConfiguration" else { return }
         
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        
-        window?.rootViewController = RootPageViewController()
-        window?.windowScene = windowScene
-        window?.makeKeyAndVisible()
     }
 }
