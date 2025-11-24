97c5619e513f238e1046362eea278be9b957dbf2
Jake Bromberg <jake@funlandresearch.com>
2025-11-24 14:42:16 -0800

    Revert to Swift Testing framework
    Swift Testing requires Xcode 16+.

--- WXYC/Shared/MusicShareKit/Tests/MusicShareKitTests/MusicShareKitTests.swift
+++ WXYC/Shared/MusicShareKit/Tests/MusicShareKitTests/MusicShareKitTests.swift
@@ -1,62 +1,59 @@
-import XCTest
+import Testing
 @testable import MusicShareKit
 
-final class MusicShareKitTests: XCTestCase {
+@Test func appleMusicURLParsing() async throws {
+    let service = AppleMusicService()
+    let url = URL(string: "https://music.apple.com/us/album/some-album/1234567890")!
     
-    func testAppleMusicURLParsing() {
-        let service = AppleMusicService()
-        let url = URL(string: "https://music.apple.com/us/album/some-album/1234567890")!
-        
-        XCTAssertTrue(service.canHandle(url: url))
-        
-        let track = service.parse(url: url)
-        XCTAssertNotNil(track)
-        XCTAssertEqual(track?.service, .appleMusic)
-    }
+    #expect(service.canHandle(url: url) == true)
     
-    func testSpotifyURLParsing() {
-        let service = SpotifyService()
-        let url = URL(string: "https://open.spotify.com/track/4iV5W9uYEdYUVa79Axb7Rh")!
-        
-        XCTAssertTrue(service.canHandle(url: url))
-        
-        let track = service.parse(url: url)
-        XCTAssertNotNil(track)
-        XCTAssertEqual(track?.service, .spotify)
-    }
+    let track = service.parse(url: url)
+    #expect(track != nil)
+    #expect(track?.service == .appleMusic)
+}
+
+@Test func spotifyURLParsing() async throws {
+    let service = SpotifyService()
+    let url = URL(string: "https://open.spotify.com/track/4iV5W9uYEdYUVa79Axb7Rh")!
     
-    func testBandcampURLParsing() {
-        let service = BandcampService()
-        let url = URL(string: "https://someartist.bandcamp.com/track/some-track")!
-        
-        XCTAssertTrue(service.canHandle(url: url))
-        
-        let track = service.parse(url: url)
-        XCTAssertNotNil(track)
-        XCTAssertEqual(track?.service, .bandcamp)
-        XCTAssertEqual(track?.artist, "Someartist")
-    }
+    #expect(service.canHandle(url: url) == true)
     
-    func testYouTubeMusicURLParsing() {
-        let service = YouTubeMusicService()
-        let url = URL(string: "https://music.youtube.com/watch?v=dQw4w9WgXcQ")!
-        
-        XCTAssertTrue(service.canHandle(url: url))
-        
-        let track = service.parse(url: url)
-        XCTAssertNotNil(track)
-        XCTAssertEqual(track?.service, .youtubeMusic)
-        XCTAssertEqual(track?.identifier, "dQw4w9WgXcQ")
-    }
+    let track = service.parse(url: url)
+    #expect(track != nil)
+    #expect(track?.service == .spotify)
+}
+
+@Test func bandcampURLParsing() async throws {
+    let service = BandcampService()
+    let url = URL(string: "https://someartist.bandcamp.com/track/some-track")!
     
-    func testSoundCloudURLParsing() {
-        let service = SoundCloudService()
-        let url = URL(string: "https://soundcloud.com/some-artist/some-track")!
-        
-        XCTAssertTrue(service.canHandle(url: url))
-        
-        let track = service.parse(url: url)
-        XCTAssertNotNil(track)
-        XCTAssertEqual(track?.service, .soundcloud)
-    }
+    #expect(service.canHandle(url: url) == true)
+    
+    let track = service.parse(url: url)
+    #expect(track != nil)
+    #expect(track?.service == .bandcamp)
+    #expect(track?.artist == "Someartist")
+}
+
+@Test func youTubeMusicURLParsing() async throws {
+    let service = YouTubeMusicService()
+    let url = URL(string: "https://music.youtube.com/watch?v=dQw4w9WgXcQ")!
+    
+    #expect(service.canHandle(url: url) == true)
+    
+    let track = service.parse(url: url)
+    #expect(track != nil)
+    #expect(track?.service == .youtubeMusic)
+    #expect(track?.identifier == "dQw4w9WgXcQ")
+}
+
+@Test func soundCloudURLParsing() async throws {
+    let service = SoundCloudService()
+    let url = URL(string: "https://soundcloud.com/some-artist/some-track")!
+    
+    #expect(service.canHandle(url: url) == true)
+    
+    let track = service.parse(url: url)
+    #expect(track != nil)
+    #expect(track?.service == .soundcloud)
 }
