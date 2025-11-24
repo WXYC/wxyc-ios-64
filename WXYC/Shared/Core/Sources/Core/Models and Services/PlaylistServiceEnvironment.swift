c965877b2dc421977f2546521f3cfdac300090b4
Jake Bromberg <jake@funlandresearch.com>
2025-11-24 09:28:12 -0800

    Fixing WatchXYC

--- WXYC/Shared/Core/Sources/Core/Models and Services/PlaylistServiceEnvironment.swift
+++ WXYC/Shared/Core/Sources/Core/Models and Services/PlaylistServiceEnvironment.swift
@@ -13,12 +13,21 @@ private struct PlaylistServiceKey: EnvironmentKey {
     static let defaultValue: PlaylistService? = nil
 }
 
+private struct ArtworkServiceKey: EnvironmentKey {
+    static let defaultValue: MultisourceArtworkService? = nil
+}
+
 // MARK: - Environment Values Extension
 
 public extension EnvironmentValues {
     var playlistService: PlaylistService? {
         get { self[PlaylistServiceKey.self] }
         set { self[PlaylistServiceKey.self] = newValue }
+    }
+    
+    var artworkService: MultisourceArtworkService? {
+        get { self[ArtworkServiceKey.self] }
+        set { self[ArtworkServiceKey.self] = newValue }
     }
 }
 
