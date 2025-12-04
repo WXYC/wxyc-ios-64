a9de5de6ccd80f6234d182cf890b79a7760c34bf
Jake Bromberg <jake@funlandresearch.com>
2025-12-04 09:23:05 -0800

    Fix PlaylistFetcher to use network request instead of local file
    Restores proper network fetching for playlist data instead of reading
    from local file system.

--- WXYC/Shared/Core/Sources/Core/Models and Services/Playlist Service/PlaylistFetcher.swift
+++ WXYC/Shared/Core/Sources/Core/Models and Services/Playlist Service/PlaylistFetcher.swift
@@ -17,9 +17,7 @@ private let decoder = JSONDecoder()
 extension URLSession: PlaylistFetcher {
     public func getPlaylist() async throws -> Playlist {
         do {
-            let playlistData = try Data.init(contentsOf: URL.WXYCPlaylist)
-//            let playlistData = playlistString.data(using: .utf8)
-//            let (playlistData, _) = try await self.data(from: URL.WXYCPlaylist)
+            let (playlistData, _) = try await self.data(from: URL.WXYCPlaylist)
             return try decoder.decode(Playlist.self, from: playlistData)
         } catch let error as NSError {
             print(error.localizedDescription)
