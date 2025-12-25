cd152f88b8f6812ba0b9a0f08228acdfb042525c
Jake Bromberg <jake@funlandresearch.com>
2025-12-25 07:42:13 -0800

    Replace SwiftNIO with URLSession in MiniMP3Streamer
    Rewrites HTTPStreamClient to use URLSession.bytes(for:) instead of
    SwiftNIO for HTTP streaming. This removes the swift-nio and swift-nio-ssl
    dependencies from the MiniMP3Streamer package.
    
    Also updates player display names and descriptions to reflect that
    both AVAudioStreamer and MiniMP3Streamer now use URLSession instead
    of SwiftNIO.

--- Shared/Playback/MiniMP3Streamer/Package.swift
+++ Shared/Playback/MiniMP3Streamer/Package.swift
@@ -16,10 +16,6 @@ let package = Package(
             name: "MiniMP3Streamer",
             targets: ["MiniMP3Streamer"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.26.0"),
-    ],
     targets: [
         .target(
             name: "CMiniMP3",
@@ -33,10 +29,6 @@ let package = Package(
             name: "MiniMP3Streamer",
             dependencies: [
                 "CMiniMP3",
-                .product(name: "NIOCore", package: "swift-nio"),
-                .product(name: "NIOPosix", package: "swift-nio"),
-                .product(name: "NIOHTTP1", package: "swift-nio"),
-                .product(name: "NIOSSL", package: "swift-nio-ssl"),
             ],
             path: "Sources/MiniMP3Streamer"),
         .testTarget(
